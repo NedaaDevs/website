2fb859a22d512631adb88792dffef4edcff243a9
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-31 14:00:37 +0300

    chore: Setup icons for vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,13 +1,20 @@
 // Styles
 import 'vuetify/styles'
 
-// Composables
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
 const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
 
 export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   theme: {
-    defaultTheme: userPrefersDark ? 'dark' : 'light',
+    defaultTheme: !userPrefersDark ? 'dark' : 'light',
   },
 })
