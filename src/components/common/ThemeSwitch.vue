b94493a077198101f55df8efc111405fcfd321df
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-31 15:03:15 +0300

    feat: Add Theme Switch component

---
+++ src/components/common/ThemeSwitch.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
+import { useAppStore } from '@/stores/app'
+
+import { mdiWeatherNight, mdiWeatherSunny } from '@mdi/js'
+
+const { toggleTheme } = useAppStore()
+
+const { theme } = storeToRefs(useAppStore())
+</script>
+
+<template>
+  <VBtn icon variant="text" @click="toggleTheme">
+    <VIcon :icon="theme === 'light' ? mdiWeatherNight : mdiWeatherSunny" />
+  </VBtn>
+</template>
