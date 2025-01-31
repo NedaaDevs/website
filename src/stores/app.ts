b265c2f926701f6f36fc0ca27c3020c3fb232b19
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-31 15:02:26 +0300

    feat: Add app store with set locale and theme toggle actions

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,8 +1,38 @@
-// Utilities
-import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useAppStore = defineStore('app', {
-  state: () => ({
-    //
-  }),
-})
+import { defineStore } from 'pinia'
+import { useLocale, useTheme } from 'vuetify'
+import { useI18n } from 'vue-i18n'
+
+export const useAppStore = defineStore(
+  'app',
+  () => {
+    const { locale: i18nLocale, t } = useI18n()
+    const { current: vuetifyLocale } = useLocale()
+    const vuetifyTheme = useTheme()
+
+    const theme = ref(vuetifyTheme.global.name.value ?? 'light')
+    const locale = ref('en')
+
+    const toggleTheme = () => {
+      theme.value = theme.value === 'light' ? 'dark' : 'light'
+      vuetifyTheme.global.name.value = theme.value
+    }
+
+    const setLocale = (newLocale: string) => {
+      locale.value = newLocale
+      i18nLocale.value = locale.value
+      vuetifyLocale.value = locale.value
+    }
+
+    return {
+      theme,
+      locale,
+      toggleTheme,
+      setLocale,
+    }
+  },
+  {
+    persist: true,
+  },
+)
