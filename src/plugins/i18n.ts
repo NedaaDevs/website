4281a15897d9a44b5c10afefcfd2ac5572c8b4a5
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-31 08:33:07 +0300

    feat: Configure and use i18n plugin

---
+++ src/plugins/i18n.ts
@@ -0,0 +1,25 @@
+import { createI18n, type I18nOptions } from 'vue-i18n'
+
+// Locales
+import ar from '@/localization/ar.json'
+import en from '@/localization/en.json'
+import ms from '@/localization/ms.json'
+
+const browserLocale = window.navigator.language.slice(0, 2) // take only locale e.g en from en-US
+
+const messages = {
+  ar,
+  en,
+  ms,
+}
+
+const options: I18nOptions = {
+  legacy: false,
+  locale: browserLocale,
+  fallbackLocale: 'en',
+  messages,
+  missingWarn: false,
+  fallbackWarn: false,
+}
+
+export default createI18n<false, typeof options>(options)
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,10 +2,11 @@
 import vuetify from '@/plugins/vuetify'
 import pinia from '@/stores'
 import router from '@/router'
+import i18n from '@/plugins/i18n'
 
 // Types
 import type { App } from 'vue'
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(router).use(pinia)
+  app.use(i18n).use(vuetify).use(router).use(pinia)
 }
