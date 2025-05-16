be3d899529f96e57fa7134ade44752e7a52e2199
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-16 14:10:18 +0300

    feat: Add PageContainer component with responsive layout

---
+++ src/components/layout/PageContainer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+type Props = {
+  fluid?: boolean
+  maxWidth?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  fluid: false,
+  maxWidth: '1200px',
+})
+</script>
+
+<template>
+  <VContainer :fluid="props.fluid" :max-width="maxWidth" class="px-md-6" v-bind="$attrs">
+    <slot />
+  </VContainer>
+</template>
