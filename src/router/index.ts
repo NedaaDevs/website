849f9a616ac75bce92a6ebbb58fd1820893540f5
Fahad <42780409+F2had@users.noreply.github.com>
2025-05-30 18:31:20 +0300

    fix: Lazy load PrivacyPolicyView component for improved performance

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
-import PrivacyPolicyView from '@/views/PrivacyPolicyView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +13,7 @@ const router = createRouter({
     {
       path: '/privacy',
       name: 'privacy',
-      component: PrivacyPolicyView,
+      component: () => import('@/views/PrivacyPolicyView.vue'),
     },
   ],
 })
